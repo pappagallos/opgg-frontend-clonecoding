69e98711128dedfdd8d50fe0c48cc7e61ab14b65
WOOJIN, LEE <pappagallos@kakao.com>
2021-10-31 15:35:06 +0900

    검색 패널의 기능인 최근검색 뷰, 즐겨찾기 뷰와 기능 개발

--- components/OPGGSearchPanel/OPGGSearchPanel.jsx
+++ components/OPGGSearchPanel/OPGGSearchPanel.jsx
@@ -78,9 +78,6 @@ const SummonerDetailArea = styled.div`
     justify-content: center;
 
     p {
-        margin: 0;
-        padding: 0;
-
         b {
             color: #d53f3f;
             font-weight: normal;
@@ -101,7 +98,7 @@ const SummonerDetailArea = styled.div`
     }
 `;
 
-const OPGGSearchTabBox = styled.div`
+const OPGGSearchTabArea = styled.div`
     position: absolute;
     top: 36px;
     left: 0;
@@ -141,24 +138,58 @@ const OPGGTabDetailArea = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
+`;
+
+const OPGGTabDetail = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 0;
+    padding: 0;
+
+    padding: 20px;
+    line-height: 15px;
+    text-align: center;
+    font-size: 12px;
+    color: #666;
+
+    span {
+        margin-top: 16px;
+    }
+`;
+
+const OPGGTabSummonerDetail = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    width: 100%;
+    padding: 0 15px;
+    margin-top: 15px;
+    box-sizing: border-box;
+    line-height: 15px;
+
+    font-size: 12px;
+    color: #666;
+
+    &:last-child {
+        margin-bottom: 15px;
+    }
 
     p {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        margin: 0;
-        padding: 0;
+        cursor: pointer;
+    }
+`;
 
-        padding: 20px;
-        line-height: 15px;
-        text-align: center;
-        font-size: 12px;
-        color: #666;
+const OPGGTabSummonerDetailController = styled.div`
+    width: auto;
+    height: inherit;
+    margin: 0;
+    padding: 0;
 
-        span {
-            margin-top: 16px;
-        }
+    img {
+        cursor: pointer;
     }
 `;
 
@@ -167,15 +198,24 @@ const OPGGSearchPanel = () => {
     const [searchKeyword, setSearchKeyword] = useState('');
     const [searchJson, setSearchJson] = useState();
     const [searchTab, setSearchTab] = useState({ key: 'latest', isOpen: false });
+    const [latestSearchSummonerList, setLatestSearchSummonerList] = useState([]);
+    const [favoriteSearchSummonerList, setFavoriteSearchSummonerList] = useState([]);
 
     useEffect(() => {
+        // 페이지가 렌더링되면 로컬 스토리지에 저장된 최근검색 소환사와 즐겨찾기 소환사를 불러오기
+        const latestSummonerList = JSON.parse(localStorage.getItem('latest_summoner')) || [];
+        const favoriteSummonerList = JSON.parse(localStorage.getItem('favorite_summoner')) || [];
 
+        setLatestSearchSummonerList(latestSummonerList);
+        setFavoriteSearchSummonerList(favoriteSummonerList);
+    }, []);
 
-    }, [searchKeyword]);
-
+    /************************************************************
+     * 이벤트 핸들러
+     ************************************************************/
     const eventHandler = {
         onChangeSearch: (keyword) => {
-            // 컴포넌트 성능 최적화를 위해 debounce 원리를 이용하여 소환사 이름을 모두 입력한 뒤 0.5초 뒤에 API 호출하도록 반영하였습니다.
+            // 컴포넌트 성능 최적화를 위해 debounce 원리를 이용하여 소환사 이름을 모두 입력한 뒤 0.3초 뒤에 API 호출하도록 반영하였습니다.
             if (debounceTimer) {
                 clearTimeout(debounceTimer);
             }
@@ -200,6 +240,12 @@ const OPGGSearchPanel = () => {
             setDebounceTimer(debounce);
         },
 
+        onClickSummoner: (summoner) => {
+            console.log(summoner);
+            eventHandler.addLatestSummoner(summoner);
+        },
+
+        // 탭 검색 박스 열기
         openSearchTab: () => {
             if (searchJson 
                 && searchJson.sections.length > 0 
@@ -213,6 +259,7 @@ const OPGGSearchPanel = () => {
             });
         },
 
+        // 탭 검색 박스 닫기
         closeSearchTab: () => {
             setSearchTab({
                 ...searchTab,
@@ -220,23 +267,66 @@ const OPGGSearchPanel = () => {
             });
         },
 
-        addLatestSummoner: () => {
+        // 최근검색에서 소환사 추가
+        addLatestSummoner: (summoner) => {
+            const latestSummonerList = JSON.parse(localStorage.getItem('latest_summoner')) || [];
+            const isExist = latestSummonerList.filter(filterSummoner => filterSummoner.name === summoner.name).length > 0;
             
+            if (isExist) {
+                return;
+            };
+
+            latestSummonerList.push(summoner);
+            setLatestSearchSummonerList(latestSummonerList);
+
+            localStorage.setItem('latest_summoner', JSON.stringify(latestSummonerList));
         },
 
-        removeLatestSummoner: () => {
-            
+        // 최근검색에서 소환사 제거
+        removeLatestSummoner: (summoner) => {
+            const latestSummonerList = JSON.parse(localStorage.getItem('latest_summoner'));
+
+            if (!latestSummonerList) {
+                return;
+            }
+
+            const targetIndex = latestSummonerList.findIndex(compareSummoner => summoner.name === compareSummoner.name);
+
+            latestSummonerList.splice(targetIndex, 1);
+            setLatestSearchSummonerList(latestSummonerList);
+
+            localStorage.setItem('latest_summoner', JSON.stringify(latestSummonerList));
         },
 
-        addFavoriteSummoner: () => {
+        // 즐겨찾기에 소환사 추가 및 삭제
+        blurFavoriteSummoner: (summoner) => {
+            const favoriteSummonerList = JSON.parse(localStorage.getItem('favorite_summoner')) || [];
+            const isExist = favoriteSummonerList.filter(filterSummoner => filterSummoner.name === summoner.name).length > 0;
 
-        }, 
+            // 소환사 있을 경우 즐겨찾기 에서 삭제
+            if (isExist) {
+                const targetIndex = favoriteSummonerList.findIndex(compareSummoner => summoner.name === compareSummoner.name);
 
-        removeFavoriteSummoner: () => {
+                favoriteSummonerList.splice(targetIndex, 1);
+                setFavoriteSearchSummonerList(favoriteSummonerList);
 
-        }
+                localStorage.setItem('favorite_summoner', JSON.stringify(favoriteSummonerList));
+            
+            // 소환사 없을 경우 즐겨찾기 에서 추가
+            } else {
+                favoriteSummonerList.push(summoner);
+                setFavoriteSearchSummonerList(favoriteSummonerList);
+
+                localStorage.setItem('favorite_summoner', JSON.stringify(favoriteSummonerList));
+            }
+
+            
+        },
     }
 
+    /************************************************************
+     * API 리스트
+     ************************************************************/
     const apiList = {
         // 검색 시 관련 소환사 자동완성 API
         fetchSummonerList: async (keyword) => {
@@ -277,7 +367,7 @@ const OPGGSearchPanel = () => {
                     <>
                         <OPGGSummonerList>
                             { searchJson.sections[0].groups[0].items.map((summoner) => 
-                                    <OPGGSummonerItem key={summoner.name}>
+                                    <OPGGSummonerItem key={summoner.name} onClick={() => eventHandler.onClickSummoner(summoner)}>
                                         <SummonerProfileIcon src={summoner.profileIconUrl} alt={summoner.name} />
                                         <SummonerDetailArea>
                                             <p dangerouslySetInnerHTML={{ __html: summoner.keyword }}></p>
@@ -292,7 +382,7 @@ const OPGGSearchPanel = () => {
 
             {/* 최근검색 및 즐겨찾기 */}
             { searchTab.isOpen &&
-                <OPGGSearchTabBox>
+                <OPGGSearchTabArea>
                     <OPGGTab>
                         <OPGGTabItem 
                             className={searchTab.key === 'latest' ? 'selected' : '' } 
@@ -307,19 +397,64 @@ const OPGGSearchPanel = () => {
                     </OPGGTab>
                     <OPGGTabDetailArea>
                         {   
-                            searchTab.key === 'latest' ? 
-                            <p>
-                                <Image src='/assets/images/icon-history-info.png' width={16} height={16} alt='latest' />
-                                <span>최근에 본 소환사가 없습니다.</span>
-                            </p>
-                            :
-                            <p>
-                                <Image src='/assets/images/icon-history-info.png' width={16} height={16} alt='favorite' />
-                                <span>관심있는 소환사에 <Image src='/assets/images/icon-favorite-off.png' width={16} height={16} alt='favorite' /> 즐겨찾기를 하여 편리하게 정보를 받아보세요</span>
-                            </p>
+                            (() => {
+                                // 최근검색 리스트가 비어있을 경우
+                                if (searchTab.key === 'latest' && latestSearchSummonerList.length <= 0) {
+                                    return (
+                                    <OPGGTabDetail>
+                                        <Image src='/assets/images/icon-history-info.png' width={16} height={16} alt='latest' />
+                                        <span>최근에 본 소환사가 없습니다.</span>
+                                    </OPGGTabDetail>
+                                    );
+                                
+                                // 최근검색 리스트가 1개 이상 존재할 경우
+                                } else if (searchTab.key === 'latest' && latestSearchSummonerList.length > 0) {
+                                    return (
+                                        latestSearchSummonerList.map(summoner => 
+                                            <OPGGTabSummonerDetail key={summoner.name}>
+                                                <p onClick={() => eventHandler.addLatestSummoner(summoner)}>{summoner.name}</p>
+
+                                                {/* 즐겨찾기에 추가되었을 경우 파란별, 추가 안되어 있을 경우 회색별 아이콘 보여주기 */}
+                                                <OPGGTabSummonerDetailController>
+                                                { favoriteSearchSummonerList.filter(favoriteSummoner => favoriteSummoner.name === summoner.name).length > 0 ?
+                                                    <Image src='/assets/images/icon-favorite-on.png' alt='즐겨찾기' width={16} height={16} onClick={() => eventHandler.blurFavoriteSummoner(summoner)} />
+                                                    :
+                                                    <Image src='/assets/images/icon-favorite-off.png' alt='즐겨찾기' width={16} height={16} onClick={() => eventHandler.blurFavoriteSummoner(summoner)} />
+                                                }
+
+                                                <Image src='/assets/images/icon-history-delete.png' alt='제거' width={16} height={16} onClick={() => eventHandler.removeLatestSummoner(summoner)} />
+                                                </OPGGTabSummonerDetailController>
+                                            </OPGGTabSummonerDetail>
+                                        )
+                                    );
+                                }
+
+                                // 즐겨찾기 리스트가 비어있을 경우
+                                if (searchTab.key === 'favorite' && favoriteSearchSummonerList.length <= 0) {
+                                    return (
+                                        <OPGGTabDetail>
+                                            <Image src='/assets/images/icon-history-info.png' width={16} height={16} alt='favorite' />
+                                            <span>관심있는 소환사에 <Image src='/assets/images/icon-favorite-off.png' width={16} height={16} alt='favorite' /> 즐겨찾기를 하여 편리하게 정보를 받아보세요</span>
+                                        </OPGGTabDetail>
+                                    );
+
+                                // 즐겨찾기 리스트가 1개 이상 존재할 경우
+                                } else if (searchTab.key === 'favorite' && favoriteSearchSummonerList.length > 0) {
+                                    return (
+                                        favoriteSearchSummonerList.map(summoner => 
+                                            <OPGGTabSummonerDetail key={summoner.name}>
+                                                <p onClick={() => eventHandler.addLatestSummoner(summoner)}>{summoner.name}</p>
+                                                <OPGGTabSummonerDetailController>
+                                                    <Image src='/assets/images/icon-history-delete.png' alt='제거' width={16} height={16} onClick={() => eventHandler.blurFavoriteSummoner(summoner)} />
+                                                </OPGGTabSummonerDetailController>
+                                            </OPGGTabSummonerDetail>
+                                        )
+                                    );
+                                }
+                            })()
                         }
                     </OPGGTabDetailArea>
-                </OPGGSearchTabBox>
+                </OPGGSearchTabArea>
             }
         </OPGGSearchArea>
     );
